8c8c99353267ae632ccff27307ee92ba5a408faf
Lucian Young <st10039287@vcconnect.edu.za>
2023-09-08 08:46:58 +0000

    Initial_commit
    Main and Information Windows designed with a view backend elements to add.

---
+++ StudyGuideApp/InformationWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudyGuideApp
+{
+    /// <summary>
+    /// Interaction logic for InformationWindow.xaml
+    /// </summary>
+    public partial class InformationWindow : Window
+    {
+        public InformationWindow()
+        {
+            InitializeComponent();
+        }
+
+        //return button
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //object of the info window
+            MainWindow window = new MainWindow();
+
+            //displays info window
+            window.Show();
+
+            //hides current window
+            Close();
+        }
+    }
+}
---
+++ StudyGuideApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudyGuideApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void beginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //info button
+        private void infoButton_Click(object sender, RoutedEventArgs e)
+        {
+            //object of the info window
+            InformationWindow window = new InformationWindow();
+
+            //displays info window
+            window.Show();
+
+            //hides current window
+            Close();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        //info button
+
+    }
+}
